abde1b594100da9824cc4833c33fac7511c3c17b
Jascha030 <contact@jaschavanaalst.nl>
2021-06-25 03:19:45 +0200

    Add interface for flexibility

--- src/Templater/TwigTemplaterInterface.php
+++ src/Templater/TwigTemplaterInterface.php
@@ -1,8 +1,22 @@
 <?php
 
+use Twig\Environment;
+
+/**
+ * Interface TwigTemplaterInterface
+ */
 interface TwigTemplaterInterface
 {
     /**
+     * Ensure a twig environment is available for rendering.
+     *
+     * @return \Twig\Environment
+     */
+    public function getEnvironment(): Environment;
+
+    /**
+     * Should wrap the environments render() method.
+     *
      * @throws \Twig\Error\SyntaxError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\LoaderError
