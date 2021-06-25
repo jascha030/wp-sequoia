532c2b611514512366b838b1a7d587c65e8a716c
Jascha030 <contact@jaschavanaalst.nl>
2021-06-25 20:39:14 +0200

    Edits due to tests failing

---
+++ tests/Component/TwigComponentAbstractTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\Tests\Sequoia\Component;
+
+use Jascha030\Sequoia\Component\TwigComponentAbstract;
+use PHPUnit\Framework\TestCase;
+
+class TwigComponentAbstractTest extends TestCase
+{
+
+}
---
+++ tests/Fixtures/Component/HelloWorldComponent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jascha030\Tests\Sequoia\Fixtures\Component;
+
+class HelloWorldComponent
+{
+
+}
