62a3948c24849d3e4980d7becdb2103b9efc1183
Jascha030 <contact@jaschavanaalst.nl>
2021-06-25 20:03:15 +0200

    Unit tests for TemplaterInterface

--- tests/Templater/TwigTemplaterTest.php
+++ tests/Templater/TwigTemplaterTest.php
@@ -2,10 +2,55 @@
 
 namespace Jascha030\Tests\Sequoia\Templater;
 
+use Jascha030\Sequoia\Templater\TwigTemplater;
 use Jascha030\Sequoia\Templater\TwigTemplaterInterface;
 use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-class TwigTemplaterTest extends TestCase
+final class TwigTemplaterTest extends TestCase
 {
+    public function testConstructor(): TwigTemplaterInterface
+    {
+        $loader      = new FilesystemLoader(dirname(__DIR__) . '/Fixtures/Templates');
+        $environment = new Environment($loader);
 
+        $templater = new TwigTemplater($environment);
+        self::assertInstanceOf(TwigTemplaterInterface::class, $templater);
+
+        return $templater;
+    }
+
+    /**
+     * @noinspection UnnecessaryAssertionInspection
+     * @depends      testConstructor
+     *
+     * @param \Jascha030\Sequoia\Templater\TwigTemplaterInterface $templater
+     */
+    public function testGetEnvironment(TwigTemplaterInterface $templater): void
+    {
+        self::assertInstanceOf(Environment::class, $templater->getEnvironment());
+    }
+
+    /**
+     * @depends testConstructor
+     *
+     * @param \Jascha030\Sequoia\Templater\TwigTemplaterInterface $templater
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function testRender(TwigTemplaterInterface $templater): void
+    {
+        $output = $templater->render(
+            'test-hello-world.twig',
+            [
+                'greeting' => 'hello',
+                'location' => 'world'
+            ]
+        );
+
+        self::assertEquals('<p>hello world!</p>', $output);
+    }
 }
