88b0c3c124fdd6c059d75dafb337776981536c2c
Jascha030 <contact@jaschavanaalst.nl>
2021-06-25 20:02:53 +0200

    Simplify gitignore

---
+++ tests/Templater/TwigTemplaterTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\Tests\Sequoia\Templater;
+
+use Jascha030\Sequoia\Templater\TwigTemplaterInterface;
+use PHPUnit\Framework\TestCase;
+
+class TwigTemplaterTest extends TestCase
+{
+
+}
